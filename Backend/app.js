1a1868999dc850240a5621c07be28cf41b994cdd
LucasTanWL <43462435+LucasTanWL@users.noreply.github.com>
2022-07-22 11:35:40 +0800

    Backend database connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //DB connect
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'Password123456',
+    database: ''
+});
 
 //to fix CORS erros
 app.use((req, res, next) =>{
@@ -18,8 +25,22 @@ app.use((req, res, next) =>{
     next();
 });
 
-//Routing
+//Error when page is not found
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
 
-//Error
+//Handling of all error
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+     res.json({
+        error: {
+            message: error.message,
+            message2: rows
+        }
+    });
+});
 
 module.exports = app;
